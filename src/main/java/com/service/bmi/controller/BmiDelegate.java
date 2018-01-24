45d953d58f14967dff5bd2b2a21088320b273eb5
paas <paas@cse-codegen-backend-1638194887-2bs20>
2018-01-24 08:04:43 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/bmi/controller/BmiDelegate.java
@@ -0,0 +1,14 @@
+package com.service.bmi.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BmiDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
