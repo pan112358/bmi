1fe76143789e9267f2b370778234c680d806e08f
pan112358 <35713208+pan112358@users.noreply.github.com>
2018-01-25 14:03:22 +0800

    Update BmiImpl.java

--- src/main/java/com/service/bmi/controller/BmiImpl.java
+++ src/main/java/com/service/bmi/controller/BmiImpl.java
@@ -14,15 +14,12 @@ import io.servicecomb.provider.rest.common.RestSchema;
 public class BmiImpl {
 
     @Autowired
-    private BmiDelegate userBmiDelegate;
+    private BmiDelegate bmiDelegate;
 
 
-    @RequestMapping(value = "/helloworld",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
-    public String helloworld( @RequestParam(value = "name", required = true) String name){
+    public String helloworld(String name, String height, String weight) {
 
-        return userBmiDelegate.helloworld(name);
+        return bmiDelegate.helloworld(name, height, weight);
     }
 
 }
